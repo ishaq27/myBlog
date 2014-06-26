6dd86e67e15048eba8353627ee779bef09b5883e
Muhammad Ishaq <bs080400339@vu.edu.pk>
2014-06-27 01:55:40 +0500

    message sami formated

--- app/controllers/myblogs_controller.rb
+++ app/controllers/myblogs_controller.rb
@@ -1,6 +1,7 @@
 class MyblogsController < ApplicationController
   def show
-    @myblogs = Blog.all
+    
+
   end
 
   def new
@@ -8,7 +9,23 @@ class MyblogsController < ApplicationController
   end
 
   def create
-    @myblog = Blog.new
+    @myblog = Blog.new myblog_params
+
+    #puts 'xxxxxxxxxxxxx', params[:user_id].inspect
+   #mmmm
+     respond_to do |format|
+      if @myblog.save
+
+        format.html { redirect_to myblogs_path, notice: 'message  posted.' }
+        format.json { render :show, status: :posted, location: @myblogs }
+      else
+        format.html { render :new }
+        format.json { render json: @myblog.errors, status: :unprocessable_entity }
+
+     end
+        
+    end
+
   end
 
   def update
@@ -18,5 +35,16 @@ class MyblogsController < ApplicationController
   end
 
   def index
+    @myblogs = Blog.all
+  end
+
+  def myblog_params
+    params.require(:blog).permit(:message, :user_type, :user_id)
+  end
+
+  private
+
+  def set_myblog
+    @myblog= Blog.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'myblogs/index'
   resources 'myblogs'
 
 #  get 'myblogs/show'
